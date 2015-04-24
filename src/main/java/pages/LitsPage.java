71647e6065c715345717650a96b717ba83bc641c
Ihor Bovkit <ihor.bovkit@gmail.com>
2015-04-24 03:21:29 +0300

    Updated HomePage with lates Nodes

--- src/main/java/pages/LitsPage.java
+++ src/main/java/pages/LitsPage.java
@@ -1,12 +1,34 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class LitsPage extends Page{
 
 	public LitsPage(WebDriver webDriver) {
 		super(webDriver);
 		
+	}
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='header-logo-image']//img")
+	public WebElement headerLogoImage;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id ='header-right-section']//i[@class='icon-mail']")
+	public WebElement mailIcon;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id ='header-right-section']//i[@class='icon-fb']")
+	public WebElement faceBookIcon;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id ='header-right-section']//i[@class='icon-moodle']")
+	public WebElement moodleIcon;
+
+	@FindBy(how = How.XPATH, using = "//*[@id ='header-right-section']//i[@class='icon-tel']")
+	public WebElement telephoneIcon;
+	
+	public boolean isAvatarDisplayed() {
+		return headerLogoImage.isDisplayed();
 	}
 
 }
